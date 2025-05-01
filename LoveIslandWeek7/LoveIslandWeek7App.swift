641c3c2f42731fe450fe67675df1abf1e36abbc5
Daniel <tackied@merrimack.edu>
2025-05-01 15:41:30 -0400

    Initial Commit

---
+++ LoveIslandWeek7/LoveIslandWeek7App.swift
@@ -0,0 +1,17 @@
+//
+//  LoveIslandWeek7App.swift
+//  LoveIslandWeek7
+//
+//  Created by Felix Otto on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LoveIslandWeek7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
